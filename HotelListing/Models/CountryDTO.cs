ad6c8aa3ae634d2ed7f31eb1893b53bcfc8450da
pavel <pavel.glos1@gmail.com>
2021-05-24 14:37:35 +0200

    Added POST, PUT, DELETE

--- HotelListing/Models/CountryDTO.cs
+++ HotelListing/Models/CountryDTO.cs
@@ -16,6 +16,11 @@ namespace HotelListing.Models
         public string ShortName { get; set; }
     }
 
+    public class UpdateCountryDTO : CreateCountryDTO
+    {
+        public IList<CreateHotelDTO> Hotels { get; set; }
+    }
+
     public class CountryDTO : CreateCountryDTO
     {
         public int Id { get; set; }
--- HotelListing/Models/HotelDTO.cs
+++ HotelListing/Models/HotelDTO.cs
@@ -23,6 +23,12 @@ namespace HotelListing.Models
         [Required]
         public int CountryId { get; set; }
     }
+
+    public class UpdateHotelDTO: CreateHotelDTO
+    {
+
+    }
+
     public class HotelDTO : CreateHotelDTO
     {
         public int Id { get; set; }
